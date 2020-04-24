7cd4c14d3ca7f5cb27abf3e67b06e67b912e08cb
cyon3 <cyon3@LAPTOP-NAD0LFUO>
2020-04-24 12:31:02 +0900

    조원 commit

--- gitTest/src/com/kh/git/GitPrac.java
+++ gitTest/src/com/kh/git/GitPrac.java
@@ -1,12 +1,15 @@
 package com.kh.git;
 
-public class GitPrac {
+public class GitPrac 
+{
 
 	public static void main(String[] args)
 	{
 		System.out.println("hello");
 		System.out.println("world");
 		System.out.println("thx");
+		System.out.println("난 조원");
+		System.out.println("난 조장");
 		//잘 올라갔겠지?
 	}
 }
